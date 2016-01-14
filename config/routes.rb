405447e9d3bb89f41779c49c7c7d7eecafd51666
levinomoises <levino_moises@hotmail.com>
2016-01-14 14:14:05 -0200

    Updated landing page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 VisiteUmaEscola::Application.routes.draw do
-  get '/usuarios/minha-conta' => 'settings#edit', as: :edit_user
-  put '/usuarios' => 'settings#update', as: :update_user
-  
+  devise_for :users, controllers: { registrations: "users/registrations" }
+
   root  to:                     'home#index'
-  get 'contato'              => 'contacts#index', as: :contacts
-  get 'contato/enviar'       => 'contacts#new',   as: :new_contact
 
   get 'frontend/:template'   => 'frontend#show'
   get 'frontend'             => 'frontend#index'
