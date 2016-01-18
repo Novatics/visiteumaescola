42464b68ad19d1c916386ae95027fd814547bab3
levinomoises <levino_moises@hotmail.com>
2016-01-18 16:40:24 -0200

    Added roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+  belongs_to :role
+  before_create :set_default_role
+
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('volunteer')
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+['volunteer', 'school', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
