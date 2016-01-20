0ff12c4720a50eff59da51c5b78a806e8c2129ae
levinomoises <levino_moises@hotmail.com>
2016-01-20 13:32:25 -0200

    Updated Devise configuration to allow extra params in the signup form

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-# before_filter :configure_sign_up_params, only: [:create]
+  before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
   def index; end
 
@@ -42,9 +42,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.for(:sign_up) << :role_id
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
