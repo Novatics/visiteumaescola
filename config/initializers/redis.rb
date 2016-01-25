57d30a748f221fbe9ceb9b1ac114a2ca3be49b41
levinomoises <levino_moises@hotmail.com>
2016-01-25 15:27:07 -0200

    Modified redis file

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-if Rails.env.production? or Rails.env.staging?
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-end
+# if Rails.env.production? or Rails.env.staging?
+#   uri = URI.parse(ENV["REDISTOGO_URL"])
+#   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# end
