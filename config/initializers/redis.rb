5ec359efc0feb7c5cbe90d860717aa83588c9f10
levinomoises <levino_moises@hotmail.com>
2016-01-25 16:15:28 -0200

    Updated REDISTOGO URL

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-# if Rails.env.production? or Rails.env.staging?
-#   uri = URI.parse(ENV["REDISTOGO_URL"])
-#   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-# end
+if Rails.env.production? or Rails.env.staging?
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://redistogo:d20c6495175fc93f8b6d7955e4f47c63@viperfish.redistogo.com:9540/" )
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
