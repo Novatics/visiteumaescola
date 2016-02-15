9defcb3a31731d9999fa279403b0596b949f3616
levinomoises <levino_moises@hotmail.com>
2016-02-15 16:36:55 -0200

    Added devise alerts to signup form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-
-  def index; end
-
+  def index
+  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,60 +1,33 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
-  def index; end
 
-  # GET /resource/sign_up
   def new
-    #binding.pry
-    super
+
   end
 
   # POST /resource
   def create
+    # super
     build_resource(sign_up_params)
 
-    resource.save
-    yield resource if block_given?
-    if resource.persisted?
+    if resource.save
       if resource.active_for_authentication?
-        set_flash_message :notice, :signed_up if is_flashing_format?
+        set_flash_message :notice, :signed_up if is_navigational_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        # respond_with resource, :location => after_sign_up_path_for(resource)
+        render "home/index"
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        # respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        render "home/index"
       end
     else
-      redirect_to root_path, alert:"Email já cadastrado"
+      # clean_up_passwords resource
+      render 'home/index'
     end
   end
-
-  # GET /resource/edit
-  def edit
-    super
-  end
-
-  # PUT /resource
-  def update
-    super
-  end
-
-  # DELETE /resource
-  def destroy
-    super
-  end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -64,7 +37,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    super(resource)
+    root_path
   end
 
   # The path used after sign up for inactive accounts.
