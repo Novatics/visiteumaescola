07165cf3d654a7b47ebf6c6186af8f8eb16921f9
levinomoises <levino_moises@hotmail.com>
2016-02-24 15:42:10 -0300

    Added serialize for School and Volunteer params

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ActiveRecord::Base
   attr_accessible :name, :email, :phone, :needs
+  serialize :needs
 
   validates :name, :email, presence: true
 end
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,6 @@
 class Volunteer < ActiveRecord::Base
   attr_accessible :name, :email, :phone, :skills
+  serialize :skills
 
   validates :name, :email, presence: true
 end
