ee32efe2cd55a92033249d41ebfe2a0cd9269680
levinomoises <levino_moises@hotmail.com>
2016-06-21 10:26:51 -0300

    Change attributes permission

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,10 +1,10 @@
 class Volunteer < ActiveRecord::Base
-  # attr_protected
-  attr_accessible :name, :email, :phone, :cellphone, :state, :city, :neighbourhood, :address,
-                  :number, :skill_sport, :skill_culture, :skill_monitoring, :skill_repairs, :skill_courses,
-                  :skill_health, :skill_psycho, :skill_donations, :skill_others, :preference_kid,
-                  :preference_teenager, :preference_adult, :preference_elderly, :preference_deaf,
-                  :preference_blind, :preference_disable, :preference_physical_disable, :preference_teacher,
-                  :preference_employee, :preference_family, :preference_baby, :points
+  attr_protected
+  # attr_accessible :name, :email, :phone, :cellphone, :state, :city, :neighbourhood, :address,
+  #                 :number, :skill_sport, :skill_culture, :skill_monitoring, :skill_repairs, :skill_courses,
+  #                 :skill_health, :skill_psycho, :skill_donations, :skill_others, :preference_kid,
+  #                 :preference_teenager, :preference_adult, :preference_elderly, :preference_deaf,
+  #                 :preference_blind, :preference_disable, :preference_physical_disable, :preference_teacher,
+  #                 :preference_employee, :preference_family, :preference_baby, :points
   validates :name, :email, presence: true
 end
